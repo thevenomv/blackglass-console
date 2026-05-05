3eed437b9976e6e06c28006dbb5959809590cc5a
thevenomv <jamiesibley5@gmail.com>
2026-05-05 13:32:38 +0100

    feat(ci): post-deploy staging smoke, scheduled billing reconcile, sign-up to /welcome

--- src/app/(marketing)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(marketing)/sign-up/[[...sign-up]]/page.tsx
@@ -16,7 +16,7 @@ export default function SignUpPage() {
         routing="path"
         path="/sign-up"
         signInUrl="/sign-in"
-        forceRedirectUrl="/dashboard"
+        forceRedirectUrl="/welcome"
         appearance={{
           elements: {
             rootBox: "mx-auto",
